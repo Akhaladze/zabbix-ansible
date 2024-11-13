510fdf51b84bc91a77629ea9a2bc9a748ea35b5a
Akhaladze <4468618@gmail.com>
2024-11-13 14:33:42 +0200

    4

--- utils/ssh-passless.sh
+++ utils/ssh-passless.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 ssh-keygen && ssh-copyid gnet@10.10.100.21
+
+
+ZABBIX TOKEN
+auth_token=$(curl -s -X POST -H "Content-Type: application/json"  -d '{ "jsonrpc": "2.0", "method": "user.login", "params": { "user": "Admin",  "password": "zabbix"}, "id": 2} 'http://localhost:8080/api_jsonrpc.php | jq -r '.result')
+echo tt
